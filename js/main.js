069977182168bc06efb8a3315d93a40b48660e3c
ramziddin-frontend <ramziddintursunov82@gmail.com>
2022-12-15 12:18:56 -0500

    Add  update html, css and js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
-function name(params) {
+const elsPaymentPeriodRadio = document.querySelectorAll('[name="payment_period"]');
+const elsPricingPlanValue = document.querySelectorAll('.pricing-plan-value');
 
-}
+elsPaymentPeriodRadio.forEach(function (elPaymentPeriodRadio) {
+  elPaymentPeriodRadio.addEventListener('change', function () {
+    const period = elPaymentPeriodRadio.value;
+
+    if (period === 'annual') {
+      elsPricingPlanValue.forEach(function (elPrice) {
+        elPrice.textContent = elPrice.closest('.pricing-plan').dataset.paymentAnnual;
+      });
+    } else {
+      elsPricingPlanValue.forEach(function (elPrice) {
+        elPrice.textContent = elPrice.closest('.pricing-plan').dataset.paymentMonthly;
+      });
+    }
+  });
+});
